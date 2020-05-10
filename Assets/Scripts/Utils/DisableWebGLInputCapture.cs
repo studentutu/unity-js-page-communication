b2eaa5d86bb6df3d7189dd6f8b3ad7b94554c3b8
Patryk Galach <patryk.galach@gmail.com>
2020-05-10 12:59:13 +0200

    Disabling capturing all input by WebGL build on a page

---
+++ Assets/Scripts/Utils/DisableWebGLInputCapture.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Disable capturing whole input on the page
+/// Read more: https://docs.unity3d.com/ScriptReference/WebGLInput-captureAllKeyboardInput.html
+/// </summary>
+public class DisableWebGLInputCapture : MonoBehaviour
+{
+    void Start()
+    {
+#if UNITY_WEBGL && !UNITY_EDITOR
+        WebGLInput.captureAllKeyboardInput = false;
+#endif
+    }
+}
+
